ecbb0971025aacea4bc155cd37d86c6fb4ab7dd8
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2025-01-12 01:31:25 -0300

    add skeleton animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				shimmer: {
+					'0%': { transform: 'translateX(-100%)' },
+					'100%': { transform: 'translateX(100%)' }
+				}
+			},
+			animation: {
+				shimmer: 'shimmer 1.5s infinite'
+			}
+		},
 	},
 	plugins: [],
 }
